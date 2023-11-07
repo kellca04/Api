ec9f30ee0ea64bff325b70a1094536a0202e2c3a
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 17:10:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-// JavaScript to interact with the API
-        document.getElementById('getJokes').addEventListener('click', function () {
-            const category = document.getElementById('category').value;
-            const language = document.getElementById('language').value;
-            const number = document.getElementById('number').value;
+document.addEventListener("DOMContentLoaded", function () {
+    // JavaScript to interact with the API
+    document.getElementById('getJokes').addEventListener('click', function () {
+        const category = document.getElementById('category').value;
+        const language = document.getElementById('language').value;
+        const number = document.getElementById('number').value;
 
-            fetch(`/api/v1/jokes?category=${category}&language=${language}&number=${number}`)
-                .then(response => response.json())
-                .then(data => {
-                    const jokesDiv = document.getElementById('jokes');
-                    jokesDiv.innerHTML = JSON.stringify(data, null, 2);
+        fetch(`/api/v1/jokes?category=${category}&language=${language}&number=${number}`)
+            .then(response => response.json())
+            .then(data => {
+                const jokesDiv = document.getElementById('jokes');
+                jokesDiv.innerHTML = ''; // Clear previous jokes
+                data.forEach(joke => {
+                    const p = document.createElement('p');
+                    p.textContent = joke;
+                    jokesDiv.appendChild(p);
                 });
-        });
+            });
+    });
 
-        document.getElementById('getJokeById').addEventListener('click', function () {
-            const category = document.getElementById('category').value;
-            const language = document.getElementById('language').value;
-            const jokeId = document.getElementById('jokeId').value;
+    document.getElementById('getJokeById').addEventListener('click', function () {
+        const language = 'en'; // Use only English
+        const jokeId = document.getElementById('jokeId').value;
 
-            fetch(`/api/v1/jokes/${language}/${category}/${jokeId}`)
-                .then(response => response.json())
-                .then(data => {
-                    const jokesDiv = document.getElementById('jokes');
-                    jokesDiv.innerHTML = JSON.stringify(data, null, 2);
-                });
-        });
+        fetch(`/api/v1/jokes/${language}/neutral/${jokeId}`)
+            .then(response => response.json())
+            .then(data => {
+                const jokesDiv = document.getElementById('jokes');
+                jokesDiv.innerHTML = JSON.stringify(data, null, 2);
+            });
+    });
+});
