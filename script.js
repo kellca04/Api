9b93f353e13f590857a2baff7efa1efa97ebedbd
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 15:37:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var getRandomJokeButton = document.getElementById("getRandomJokeButton");
-    var getJokeByIdButton = document.getElementById("getJokeByIdButton");
-    var jokesContainer = document.getElementById("jokes-container");
-    var idInput = document.getElementById("idInput");
+// JavaScript to interact with the API
+        document.getElementById('getJokes').addEventListener('click', function () {
+            const category = document.getElementById('category').value;
+            const language = document.getElementById('language').value;
+            const number = document.getElementById('number').value;
 
-    getRandomJokeButton.addEventListener("click", function() {
-        var language = "en"; // Default language
-        var category = "all"; // Default category
-        var number = 1; // Default number of jokes
+            fetch(`/api/v1/jokes?category=${category}&language=${language}&number=${number}`)
+                .then(response => response.json())
+                .then(data => {
+                    const jokesDiv = document.getElementById('jokes');
+                    jokesDiv.innerHTML = JSON.stringify(data, null, 2);
+                });
+        });
 
-        fetch(`https://jokesapikellca04.onrender.com/api/v1/jokes?category=${category}&language=${language}&number=${number}`)
-            .then(handleResponse)
-            .then(function(jokes) {
-                displayJokes(jokes);
-            });
-    });
+        document.getElementById('getJokeById').addEventListener('click', function () {
+            const category = document.getElementById('category').value;
+            const language = document.getElementById('language').value;
+            const jokeId = document.getElementById('jokeId').value;
 
-    getJokeByIdButton.addEventListener("click", function() {
-        var id = idInput.value;
-
-        fetch(`https://jokesapikellca04.onrender.com/api/v1/jokes/${id}`)
-            .then(handleResponse)
-            .then(function(joke) {
-                displayJokes([joke]);
-            });
-    });
-
-    function handleResponse(response) {
-        if (!response.ok) {
-            if (response.status === 404) {
-                jokesContainer.innerHTML = "Unrealistic selections";
-            } else {
-                throw new Error("Network response was not ok");
-            }
-        }
-        return response.json();
-    }
-
-    function displayJokes(jokes) {
-        jokesContainer.innerHTML = "";
-
-        if (jokes.length === 0) {
-            jokesContainer.innerHTML = "No jokes available.";
-            return;
-        }
-
-        jokesContainer.innerHTML += "<h2>Jokes:</h2>";
-
-        for (var i = 0; i < jokes.length; i++) {
-            var joke = jokes[i];
-            jokesContainer.innerHTML +=
-                "<p><strong>ID:</strong> " + joke.id + "<br>" +
-                "<strong>Category:</strong> " + joke.category + "<br>" +
-                "<strong>Language:</strong> " + joke.language + "<br>" +
-                "<strong>Joke:</strong> " + joke.joke + "</p>";
-        }
-    }
-});
+            fetch(`/api/v1/jokes/${language}/${category}/${jokeId}`)
+                .then(response => response.json())
+                .then(data => {
+                    const jokesDiv = document.getElementById('jokes');
+                    jokesDiv.innerHTML = JSON.stringify(data, null, 2);
+                });
+        });
