76ad5dd789a1191dfd871e7a02c5d33d642fe8c4
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 17:35:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // JavaScript to interact with the API
-    document.getElementById('getJokeById').addEventListener('click', function () {
-        const jokeId = document.getElementById('jokeId').value;
+document.addEventListener('DOMContentLoaded', function () {
+    const getRandomBtn = document.getElementById('getRandom');
+    const getByIdBtn = document.getElementById('getById');
+    const resultDiv = document.getElementById('result');
 
-        fetch(`https://jokesapikellca04.onrender.com/api/v1/jokes/${jokeId}`, {
-            method: 'GET',
-            headers: {
-                'Origin': 'https://kellca04jokes.netlify.app'
-            }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const jokesDiv = document.getElementById('jokes');
-            jokesDiv.innerHTML = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Handle the error, e.g., display an error message to the user.
-        });
+    getRandomBtn.addEventListener('click', function () {
+        const language = document.getElementById('language').value;
+        const category = document.getElementById('category').value;
+        const count = document.getElementById('count').value;
+
+        fetch(`/get_jokes?language=${language}&category=${category}&count=${count}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                resultDiv.innerHTML = JSON.stringify(data, null, 2);
+            })
+            .catch(error => {
+                resultDiv.innerHTML = 'Incorrect entries!';
+            });
+    });
+
+    getByIdBtn.addEventListener('click', function () {
+        const id = document.getElementById('jokeId').value;
+        const language = document.getElementById('language').value;
+
+        fetch(`/get_joke_by_id?id=${id}&language=${language}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                resultDiv.innerHTML = JSON.stringify(data, null, 2);
+            })
+            .catch(error => {
+                resultDiv.innerHTML = 'Incorrect entries!';
+            });
     });
 });
