b0e73288ea98d4cba5da634d238bed010fe92120
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 13:33:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,72 @@
-document.getElementById('getJokes').addEventListener('click', () => {
-    const category = document.getElementById('category').value;
-    const language = document.getElementById('language').value;
-    const number = document.getElementById('number').value;
-    const jokeId = document.getElementById('jokeId').value;
+document.addEventListener("DOMContentLoaded", function() {
+    const languageSelect = document.getElementById("languageSelect");
+    const categorySelect = document.getElementById("categorySelect");
+    const numberSelect = document.getElementById("numberSelect");
+    const getJokesButton = document.getElementById("getJokesButton");
+    const jokeIdInput = document.getElementById("jokeIdInput");
+    const getJokeByIdButton = document.getElementById("getJokeByIdButton");
+    const jokesContainer = document.getElementById("jokes-container");
 
-    const apiUrl = `https://jokesapikellca04.onrender.com/api/v1/jokes?category=${category}&language=${language}&number=${number}&jokeId=${jokeId}`;
+    getJokesButton.addEventListener("click", function() {
+        const language = languageSelect.value;
+        const category = categorySelect.value;
+        const number = numberSelect.value;
 
-    fetch(apiUrl)
-        .then(response => {
-            if (response.status === 404) {
-                return "Error 404";
-            }
-            return response.json();
-        })
-        .then(data => {
-            const jokesContainer = document.getElementById('jokes');
-            jokesContainer.innerHTML = '';
-            if (data === "Error 404") {
-                jokesContainer.innerHTML = '<p class="text-danger">Error 404: Joke not found</p>';
-            } else {
-                data.forEach(joke => {
-                    jokesContainer.innerHTML += `<p>${joke}</p>`;
-                });
-            }
-        })
-        .catch(error => console.error(error));
+        fetch(`/api/v1/jokes?category=${category}&language=${language}&number=${number}`)
+            .then(response => response.json())
+            .then(data => displayJokes(data))
+            .catch(error => console.error("Error fetching jokes:", error));
+    });
+
+    getJokeByIdButton.addEventListener("click", function() {
+        const jokeId = jokeIdInput.value;
+
+        if (jokeId === "") {
+            alert("Please enter a valid joke ID.");
+            return;
+        }
+
+        fetch(`/api/v1/jokes/${jokeId}`)
+            .then(response => {
+                if (response.status === 404) {
+                    alert("Joke not found.");
+                }
+                return response.json();
+            })
+            .then(data => displayJoke(data))
+            .catch(error => console.error("Error fetching joke:", error));
+    });
+
+    function displayJokes(jokes) {
+        jokesContainer.innerHTML = "";
+
+        if (jokes.length === 0) {
+            jokesContainer.innerHTML = "No jokes available.";
+            return;
+        }
+
+        jokesContainer.innerHTML += "<h2>Jokes:</h2>";
+
+        jokes.forEach(joke => {
+            jokesContainer.innerHTML += `
+                <p><strong>ID:</strong> ${joke.id}<br>
+                <strong>Category:</strong> ${joke.category}<br>
+                <strong>Language:</strong> ${joke.language}<br>
+                <strong>Joke:</strong> ${joke.joke}</p>
+            `;
+        });
+    }
+
+    function displayJoke(joke) {
+        jokesContainer.innerHTML = "";
+
+        if (joke.id) {
+            jokesContainer.innerHTML += `
+                <p><strong>ID:</strong> ${joke.id}<br>
+                <strong>Category:</strong> ${joke.category}<br>
+                <strong>Language:</strong> ${joke.language}<br>
+                <strong>Joke:</strong> ${joke.joke}</p>
+            `;
+        }
+    }
 });
