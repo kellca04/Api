a4cd12dbfcf3e124293452516d15283cebf6ce28
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 13:27:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 import pyjokes
-import os
-os.environ['FLASK_APP'] = 'app'
-app = Flask(__name)
 
-@app.route('/', methods=['GET'])
+app = Flask(__name__)
+
+# Sample joke categories
+categories = ["all", "neutral", "chuck"]
+
+# Sample joke languages
+languages = ["en", "de", "es"]
+
+# Initialize an empty list to store jokes
+jokes = []
+
+# Counter to generate unique joke IDs
+joke_id_counter = 1
+
+# Generate jokes and populate the jokes list
+def generate_jokes():
+    global joke_id_counter
+    for lang in languages:
+        for category in categories:
+            joke_text = pyjokes.get_joke(language=lang, category=category)
+            jokes.append({
+                "id": joke_id_counter,
+                "category": category,
+                "language": lang,
+                "joke": joke_text
+            })
+            joke_id_counter += 1
+
+generate_jokes()
+
+# Route to get a list of jokes
+@app.route('/api/jokes', methods=['GET'])
 def get_jokes():
-    category = request.args.get('category')
-    language = request.args.get('language')
-    number = int(request.args.get('number'))
+    return jsonify(jokes)
 
-    jokes = pyjokes.get_jokes(category=[category], language=[language])
-    if not jokes:
-        return jsonify({'error': 'No jokes found for the specified category and language.'}), 404
-
-    if number > len(jokes):
-        return jsonify({'error': 'Number of jokes requested exceeds available jokes.'}), 400
-
-    random_jokes = jokes[:number]
-    return jsonify(random_jokes)
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
