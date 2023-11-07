e4a81be66894e086d5c44a14cb34243cb5d6f6df
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 17:24:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     // JavaScript to interact with the API
-    document.getElementById('getJokes').addEventListener('click', function () {
-        const category = document.getElementById('category').value;
-        const language = document.getElementById('language').value;
-        const number = document.getElementById('number').value;
-
-        fetch(`/api/v1/jokes?category=${category}&language=${language}&number=${number}`)
-            .then(response => response.json())
-            .then(data => {
-                const jokesDiv = document.getElementById('jokes');
-                jokesDiv.innerHTML = ''; // Clear previous jokes
-                data.forEach(joke => {
-                    const p = document.createElement('p');
-                    p.textContent = joke;
-                    jokesDiv.appendChild(p);
-                });
-            });
-    });
-
     document.getElementById('getJokeById').addEventListener('click', function () {
-        const language = 'en'; // Use only English
         const jokeId = document.getElementById('jokeId').value;
 
-        fetch(`/api/v1/jokes/${language}/neutral/${jokeId}`)
-            .then(response => response.json())
-            .then(data => {
-                const jokesDiv = document.getElementById('jokes');
-                jokesDiv.innerHTML = JSON.stringify(data, null, 2);
-            });
+        fetch(`https://jokesapikellca04.onrender.com/api/v1/jokes/${jokeId}`, {
+            method: 'GET',
+            headers: {
+                'Origin': 'https://kellca04jokes.netlify.app'
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const jokesDiv = document.getElementById('jokes');
+            jokesDiv.innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Handle the error, e.g., display an error message to the user.
+        });
     });
 });
