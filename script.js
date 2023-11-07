32fbe2d699389a498c6e3205d0fbfe202fbe6e4d
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 14:18:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var getJokesButton = document.getElementById("getJokesButton");
     var getRandomJokeButton = document.getElementById("getRandomJokeButton");
+    var searchByIdButton = document.getElementById("searchByIdButton");
     var jokesContainer = document.getElementById("jokes-container");
     var languageSelect = document.getElementById("languageSelect");
     var categorySelect = document.getElementById("categorySelect");
     var numberInput = document.getElementById("numberInput");
+    var idInput = document.getElementById("idInput");
 
     getJokesButton.addEventListener("click", function() {
         var language = languageSelect.value;
@@ -12,24 +14,9 @@ document.addEventListener("DOMContentLoaded", function() {
         var number = numberInput.value;
 
         fetch(`https://jokesapikellca04.onrender.com/api/v1/jokes?category=${category}&language=${language}&number=${number}`)
-            .then(function(response) {
-                if (!response.ok) {
-                    if (response.status === 404) {
-                        jokesContainer.innerHTML = "Unrealistic selections";
-                    } else {
-                        throw new Error("Network response was not ok");
-                    }
-                } else {
-                    return response.json();
-                }
-            })
+            .then(handleResponse)
             .then(function(jokes) {
-                if (jokes) {
-                    displayJokes(jokes);
-                }
-            })
-            .catch(function(error) {
-                console.error("Error fetching jokes:", error);
+                displayJokes(jokes);
             });
     });
 
@@ -38,26 +25,34 @@ document.addEventListener("DOMContentLoaded", function() {
         var category = categorySelect.value;
 
         fetch(`https://jokesapikellca04.onrender.com/api/v1/jokes?category=${category}&language=${language}&number=1`)
-            .then(function(response) {
-                if (!response.ok) {
-                    if (response.status === 404) {
-                        jokesContainer.innerHTML = "Unrealistic selections";
-                    } else {
-                        throw new Error("Network response was not ok");
-                    }
-                } else {
-                    return response.json();
-                }
-            })
+            .then(handleResponse)
             .then(function(jokes) {
-                if (jokes) {
-                    displayJokes(jokes);
-                }
-            })
-            .catch(function(error) {
-                console.error("Error fetching jokes:", error);
+                displayJokes(jokes);
             });
     });
+
+    searchByIdButton.addEventListener("click", function() {
+        var id = idInput.value;
+        var language = languageSelect.value;
+        var category = categorySelect.value;
+
+        fetch(`https://jokesapikellca04.onrender.com/api/v1/jokes/${language}/${category}/${id}`)
+            .then(handleResponse)
+            .then(function(joke) {
+                displayJokes([joke]);
+            });
+    });
+
+    function handleResponse(response) {
+        if (!response.ok) {
+            if (response.status === 404) {
+                jokesContainer.innerHTML = "Unrealistic selections";
+            } else {
+                throw new Error("Network response was not ok");
+            }
+        }
+        return response.json();
+    }
 
     function displayJokes(jokes) {
         jokesContainer.innerHTML = "";
@@ -73,9 +68,4 @@ document.addEventListener("DOMContentLoaded", function() {
             var joke = jokes[i];
             jokesContainer.innerHTML +=
                 "<p><strong>ID:</strong> " + joke.id + "<br>" +
-                "<strong>Category:</strong> " + joke.category + "<br>" +
-                "<strong>Language:</strong> " + joke.language + "<br>" +
-                "<strong>Joke:</strong> " + joke.joke + "</p>";
-        }
-    }
-});
+                "<strong>
