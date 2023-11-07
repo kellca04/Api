356e50062c137d8f3afa2d01ffa4c46971c4fafa
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 17:58:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import pyjokes
 
-app = Flask(__name)
+app = Flask(app)
 
 # Mock database for jokes
 jokes_db = []
@@ -16,4 +16,29 @@ def get_jokes():
     for _ in range(count):
         joke = pyjokes.get_joke(language, category)
         jokes.append({
-           
+            'ID': len(jokes_db) + 1,
+            'Joke': joke,
+            'Category': category,
+            'Language': language
+        })
+        jokes_db.append(joke)
+
+    return jsonify(jokes)
+
+@app.route('/get_joke_by_id', methods=['GET'])
+def get_joke_by_id():
+    joke_id = int(request.args.get('id'))
+    language = request.args.get('language', 'en')
+
+    if 0 < joke_id <= len(jokes_db):
+        return jsonify({
+            'ID': joke_id,
+            'Joke': jokes_db[joke_id - 1],
+            'Category': 'unknown',
+            'Language': language
+        })
+    else:
+        return jsonify({'error': 'Invalid ID'})
+
+if __name__ == '__main__':
+    app.run()
