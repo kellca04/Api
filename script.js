b45310ff48309d1ac71544568a42272ad1ca2d93
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 18:01:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const getRandomBtn = document.getElementById('getRandom');
-    const getByIdBtn = document.getElementById('getById');
-    const resultDiv = document.getElementById('result');
-
-    getRandomBtn.addEventListener('click', function () {
-        const language = document.getElementById('language').value;
-        const category = document.getElementById('category').value;
-        const count = document.getElementById('count').value;
-
-        fetch(`https://jokesapikellca04.onrender.com/get_jokes?language=${language}&category=${category}&count=${count}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                resultDiv.innerHTML = JSON.stringify(data, null, 2);
-            })
-            .catch(error => {
-                resultDiv.innerHTML = 'Incorrect entries!';
-            });
-    });
-
-    getByIdBtn.addEventListener('click', function () {
-        const id = document.getElementById('jokeId').value;
-        const language = document.getElementById('language').value;
-
-        fetch(`https://jokesapikellca04.onrender.com/get_joke_by_id?id=${id}&language=${language}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw an Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                resultDiv.innerHTML = JSON.stringify(data, null, 2);
-            })
-            .catch(error => {
-                resultDiv.innerHTML = 'Incorrect entries!';
-            });
+$(document).ready(function() {
+    $("#get-joke-button").click(function() {
+        $.ajax({
+            url: "/get_joke",
+            method: "GET",
+            success: function(response) {
+                var joke = response.joke;
+                $("#joke-text").text(joke);
+            },
+            error: function(error) {
+                console.log("Error fetching joke: " + error);
+            }
+        });
     });
 });
