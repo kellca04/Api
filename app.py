c1b19e408b819072dd9f73f2e5672886663fa1f9
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 18:01:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, jsonify
 import pyjokes
 
-app = Flask(app)
+app = Flask(__name__)
 
-# Mock database for jokes
-jokes_db = []
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route('/get_jokes', methods=['GET'])
-def get_jokes():
-    language = request.args.get('language', 'en')
-    category = request.args.get('category', 'all')
-    count = int(request.args.get('count', 1))
-
-    jokes = []
-    for _ in range(count):
-        joke = pyjokes.get_joke(language, category)
-        jokes.append({
-            'ID': len(jokes_db) + 1,
-            'Joke': joke,
-            'Category': category,
-            'Language': language
-        })
-        jokes_db.append(joke)
-
-    return jsonify(jokes)
-
-@app.route('/get_joke_by_id', methods=['GET'])
-def get_joke_by_id():
-    joke_id = int(request.args.get('id'))
-    language = request.args.get('language', 'en')
-
-    if 0 < joke_id <= len(jokes_db):
-        return jsonify({
-            'ID': joke_id,
-            'Joke': jokes_db[joke_id - 1],
-            'Category': 'unknown',
-            'Language': language
-        })
-    else:
-        return jsonify({'error': 'Invalid ID'})
+@app.route('/get_joke', methods=['GET'])
+def get_joke():
+    joke = pyjokes.get_joke()
+    return jsonify({'joke': joke})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
