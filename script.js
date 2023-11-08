56921b9beafe9b22c65d8dc23010e3ef40df6af4
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 18:26:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-$(document).ready(function() {
-    $("#get-joke-button").click(function() {
-        $.ajax({
-            url: "/get_joke",
-            method: "GET",
-            success: function(response) {
-                var joke = response.joke;
-                $("#joke-text").text(joke);
-            },
-            error: function(error) {
-                console.log("Error fetching joke: " + error);
-            }
+$(document).ready(function () {
+    const baseUrl = "https://jokesapikellca04.onrender.com/api/v1/jokes";
+
+    $("#getJokesButton").click(function () {
+        const language = $("#languageSelect").val();
+        const category = $("#categorySelect").val();
+        const number = $("#numberInput").val();
+
+        $.get(`${baseUrl}/${language}/${category}/${number}`, function (data) {
+            displayJokes(data);
         });
     });
+
+    $("#getJokeByIdButton").click(function () {
+        const language = $("#languageSelect").val();
+        const category = $("#categorySelect").val();
+        const number = 1; // Requesting a single joke
+        const jokeId = $("#jokeIdInput").val();
+
+        $.get(`${baseUrl}/${language}/${category}/${number}/${jokeId}`, function (data) {
+            displayJokes(data);
+        });
+    });
+
+    function displayJokes(data) {
+        const jokesOutput = $("#jokesOutput");
+        jokesOutput.empty();
+
+        if (Array.isArray(data.data)) {
+            data.data.forEach((joke, index) => {
+                jokesOutput.append(`<p><strong>Joke ${index + 1}:</strong> ${joke}</p>`);
+            });
+        } else if (typeof data.data === "string") {
+            jokesOutput.append(`<p>${data.data}</p>`);
+        }
+    }
 });
