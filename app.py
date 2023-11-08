9e57e3d9a5483d520ff40277dc7fd45aad1d4396
Casey Kellett Whitwham <143742524+kellca04@users.noreply.github.com>
2023-11-07 19:46:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from flask import Flask, jsonify, request, abort, send_from_directory
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS 
 import pyjokes
 import os
 
-app = Flask("app")  # Use a fixed app name
-CORS(app)  # Initialize CORS with your Flask app
+app = Flask("app") 
+CORS(app) 
 
-# Sample joke categories
+
 categories = ["all", "neutral", "chuck"]
 
-# Sample joke languages
+
 languages = ["en", "de", "es"]
 
-# Initialize an empty list to store jokes
+
 jokes = []
 
-# Counter to generate unique joke IDs
+
 joke_id_counter = 1
 
-# Generate jokes and populate the jokes list
 def generate_jokes():
     global joke_id_counter
     for lang in languages:
@@ -33,17 +32,16 @@ def generate_jokes():
                 })
                 joke_id_counter += 1
             except pyjokes.pyjokes.CategoryNotFoundError:
-                # Handle unsupported category and language combination
                 pass
 
 generate_jokes()
 
-# Serve the favicon.ico file (if you have it)
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# Route to get jokes by category, language, and number
+
 @app.route('/api/v1/jokes', methods=['GET'])
 def get_jokes():
     category = request.args.get('category')
@@ -60,7 +58,7 @@ def get_jokes():
 
     return jsonify(matching_jokes[:number])
 
-# Route to get a specific joke by ID
+
 @app.route('/api/v1/jokes/<int:joke_id>', methods=['GET'])
 def get_joke_by_id(joke_id):
     matching_jokes = [joke for joke in jokes if joke["id"] == joke_id]
